0bcc393cb6b1f8a9af7e10afce073c04dccb7330
Aris Goudouras <aris.gk@gmail.com>
2025-04-17 11:51:30 +0300

    feat: implement walkDiffTree

--- src/ProseMirror/Diff.hs
+++ src/ProseMirror/Diff.hs
@@ -2,7 +2,7 @@ module ProseMirror.Diff (proseMirrorDocWithDiffDecorations) where
 
 import Control.Monad.State (State, evalState)
 import qualified Data.Text as T
-import Data.Tree (Tree)
+import Data.Tree (Tree (..))
 import DocTree.LeafTextSpans (DocNode)
 import qualified ProseMirror.PMJson as PM (Node (..))
 import RichTextDiffOp (RichTextDiffOp)
@@ -23,4 +23,10 @@ proseMirrorDocWithDiffDecorations :: Tree (RichTextDiffOp DocNode) -> DecoratedP
 proseMirrorDocWithDiffDecorations diffTree = evalState (walkDiffTree diffTree) 0
 
 walkDiffTree :: Tree (RichTextDiffOp DocNode) -> State PMIndex DecoratedPMTree
-walkDiffTree = undefined
+walkDiffTree (Node nodeWithDiff subTrees) = do
+  pmNode <- walkDiffTreeNode nodeWithDiff
+  pmSubForest <- mapM (walkDiffTree) subTrees
+  pure $ Node {rootLabel = pmNode, subForest = pmSubForest}
+
+walkDiffTreeNode :: RichTextDiffOp DocNode -> State PMIndex (Either PM.Node (Decoration PM.Node))
+walkDiffTreeNode = undefined
