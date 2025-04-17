37207683cdb0edbadeb128ea2652702ac4a9a312
Aris Goudouras <aris.gk@gmail.com>
2025-04-17 10:31:22 +0300

    feat: introduce a state monad for capturing the PM index

--- src/ProseMirror/Diff.hs
+++ src/ProseMirror/Diff.hs
@@ -1,20 +1,26 @@
 module ProseMirror.Diff (proseMirrorDocWithDiffDecorations) where
 
+import Control.Monad.State (State, evalState)
 import qualified Data.Text as T
 import Data.Tree (Tree)
 import DocTree.LeafTextSpans (DocNode)
-import ProseMirror.PMJson (Doc, Node (..))
+import qualified ProseMirror.PMJson as PM (Node (..))
 import RichTextDiffOp (RichTextDiffOp)
 
 data DecorationAttrs = DecorationAttrs {nodeName :: Maybe T.Text, cssClass :: Maybe T.Text, style :: Maybe T.Text} deriving (Show, Eq)
 
-data InlineDecoration = PMInlineDecoration {from :: Int, to :: Int, attrs :: DecorationAttrs} deriving (Show, Eq)
+data InlineDecoration a = PMInlineDecoration {from :: Int, to :: Int, attrs :: DecorationAttrs} deriving (Show, Eq)
 
-data WidgetDecoration = PMWidgetDecoration {pos :: Int, node :: Node} deriving (Show, Eq)
+data WidgetDecoration a = PMWidgetDecoration {pos :: Int} deriving (Show, Eq)
 
-data Decoration = InlineDecoration InlineDecoration | WidgetDecoration WidgetDecoration deriving (Show, Eq)
+data Decoration a = InlineDecoration (InlineDecoration a) | WidgetDecoration (WidgetDecoration a) deriving (Show, Eq)
 
-data DocWithDecorations = DocWithDecorations {doc :: Doc, decorations :: [Decoration]} deriving (Show, Eq)
+type DecoratedPMTree = Tree (Either PM.Node (Decoration PM.Node))
 
-proseMirrorDocWithDiffDecorations :: Tree (RichTextDiffOp DocNode) -> DocWithDecorations
-proseMirrorDocWithDiffDecorations = undefined
+type PMIndex = Int
+
+proseMirrorDocWithDiffDecorations :: Tree (RichTextDiffOp DocNode) -> DecoratedPMTree
+proseMirrorDocWithDiffDecorations diffTree = evalState (walkDiffTree diffTree) 0
+
+walkDiffTree :: Tree (RichTextDiffOp DocNode) -> State PMIndex DecoratedPMTree
+walkDiffTree = undefined
