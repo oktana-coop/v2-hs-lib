8f7b4b395c3249d7c48f381060fde27dfdcaec27
Aris Goudouras <aris.gk@gmail.com>
2026-02-02 10:35:01 +0200

    feat: add pandoc tracing function

--- src/ProseMirror/Utils/Debug.hs
+++ src/ProseMirror/Utils/Debug.hs
@@ -1,7 +1,12 @@
-module ProseMirror.Utils.Debug (traceTree) where
+module ProseMirror.Utils.Debug (traceTree, tracePandoc) where
 
 import Data.Tree (Tree (..), drawTree)
-import qualified Debug.Trace
+import qualified Debug.Trace (trace)
+import Text.Pandoc (Pandoc)
+import Text.Show.Pretty (ppShow)
 
 traceTree :: (Show a) => Tree a -> Tree a
 traceTree tree = Debug.Trace.trace (drawTree $ fmap show tree) tree
+
+tracePandoc :: Pandoc -> Pandoc
+tracePandoc doc = Debug.Trace.trace (ppShow doc) doc
