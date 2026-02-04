55eb0c0e016da47c2920a41f9398da79762f5cbc
Aris Goudouras <aris.gk@gmail.com>
2026-02-04 16:37:14 +0200

    test: organize diff cases

--- test/Diff/PmOutput/Golden.hs
+++ test/Diff/PmOutput/Golden.hs
@@ -9,19 +9,28 @@ import Utils (normalizeJson)
 
 tests :: IO TestTree
 tests = do
-  return $
-    testGroup
+  return
+    $ testGroup
       "Markdown Inputs → ProseMirror Diff (Golden)"
-      [ goldenCase "add-paragraph-end",
-        goldenCase "add-paragraph-middle",
-        goldenCase "add-strong-emphasis-to-word",
-        goldenCase "add-text-in-the-middle-of-paragraph",
-        goldenCase "append-text-to-paragraph",
-        goldenCase "delete-last-paragraph",
-        goldenCase "delete-paragraph-middle",
-        goldenCase "delete-text-from-the-end-of-paragraph",
-        goldenCase "delete-text-from-the-middle-of-paragraph",
-        goldenCase "remove-strong-emphasis-from-word"
+    $ [ testGroup
+          "adding-and-removing-paragraphs"
+          [ goldenCase $ "adding-and-removing-paragraphs" </> "add-paragraph-end",
+            goldenCase $ "adding-and-removing-paragraphs" </> "add-paragraph-middle",
+            goldenCase $ "adding-and-removing-paragraphs" </> "delete-last-paragraph",
+            goldenCase $ "adding-and-removing-paragraphs" </> "delete-paragraph-middle"
+          ],
+        testGroup
+          "inline-text-updates"
+          [ goldenCase $ "inline-text-updates" </> "add-text-in-the-middle-of-paragraph",
+            goldenCase $ "inline-text-updates" </> "append-text-to-paragraph",
+            goldenCase $ "inline-text-updates" </> "delete-text-from-the-end-of-paragraph",
+            goldenCase $ "inline-text-updates" </> "delete-text-from-the-middle-of-paragraph"
+          ],
+        testGroup
+          "marks"
+          [ goldenCase $ "marks" </> "add-strong-emphasis-to-word",
+            goldenCase $ "marks" </> "remove-strong-emphasis-from-word"
+          ]
       ]
 
 goldenCase :: FilePath -> TestTree
